8ead978eba962d86c5f51064d45a14ac84957ec4
jadavravi3350 <jadavravi3350@gmail.com>
2024-08-22 13:06:07 -0400

    ok

---
+++ portfolio/js/Indext.js
@@ -0,0 +1,13 @@
+function fun1()
+{
+   if(document.getElementById("nav2").style.display == "")
+   {
+    document.getElementById("nav2").style.display = "block";
+    document.getElementById("nav").style.display = "block";
+   }
+
+   else
+   {
+    document.getElementById("nav2").style.display = "";
+   }
+}
